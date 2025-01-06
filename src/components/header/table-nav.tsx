e3335e9aaff84ab9a03775762a63cbf7c936fd78
星月なるこ <projectcsj@gmail.com>
2025-01-06 20:15:13 +0800

    feat: tablet nav

--- src/components/header/table-nav.tsx
+++ src/components/header/table-nav.tsx
@@ -1,3 +1,28 @@
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+
+import navigation from "@/data/navigation.json"
+import Link from "next/link"
+
 export default function TableNav() {
-  return <></>
+  return (
+    <NavigationMenu className="lg:hidden">
+      <NavigationMenuList>
+        {navigation.map((item, index) => (
+          <NavigationMenuItem key={index}>
+            <Link href={item.href} legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                {item.label}
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        ))}
+      </NavigationMenuList>
+    </NavigationMenu>
+  )
 }
