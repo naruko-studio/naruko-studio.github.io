1c74cf2cdeb60616f9d52f7f336a8ed8252793c7
星月なるこ <projectcsj@gmail.com>
2025-01-06 20:14:49 +0800

    feat: remove fragment

--- src/components/header/logo.tsx
+++ src/components/header/logo.tsx
@@ -3,17 +3,15 @@ import Link from "next/link"
 
 export default function Logo() {
   return (
-    <>
-      <Link href="/">
-        <Image
-          src="/logo.svg"
-          alt="なるこStudio"
-          width={24}
-          height={24}
-          className="float-left mx-2 my-1"
-        />
-        <span className="text-lg md:text-xl lg:text-2xl">なるこStudio</span>
-      </Link>
-    </>
+    <Link href="/">
+      <Image
+        src="/logo.svg"
+        alt="なるこStudio"
+        width={24}
+        height={24}
+        className="float-left mx-2 my-1"
+      />
+      <span className="text-lg md:text-xl lg:text-2xl">なるこStudio</span>
+    </Link>
   )
 }
