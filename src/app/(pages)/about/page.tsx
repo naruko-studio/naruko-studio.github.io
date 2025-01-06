8ad7189e3e05f71d7f79d8ef4b5e64de3c3a23da
星月なるこ <projectcsj@gmail.com>
2025-01-06 19:55:30 +0800

    feat: Add about us page.

--- src/app/(pages)/about/page.tsx
+++ src/app/(pages)/about/page.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next"
-
+import data from "@/data/about-us.json"
 export const metadata: Metadata = {
   title: "About Us",
 }
@@ -8,9 +8,7 @@ export default function About() {
   return (
     <>
       <h1 className="text-2xl md:text-3xl lg:text-4xl">About Us</h1>
-      <h2 className="mt-4 text-xl md:text-2xl lg:text-3xl">
-        We are a small team of developers who love to create things.
-      </h2>
+      <p className="whitespace-pre-wrap break-words">{data.text}</p>
     </>
   )
 }
