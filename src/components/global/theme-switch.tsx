9648f188f792ddd26df60ccdc782b9f7ce5fd5d7
星月なるこ <projectcsj@gmail.com>
2025-01-07 23:08:45 +0800

    feat: Add tooltip for theme-switch

--- src/components/global/theme-switch.tsx
+++ src/components/global/theme-switch.tsx
@@ -4,6 +4,12 @@ import * as React from "react"
 import { Moon, Sun } from "lucide-react"
 import { useTheme } from "next-themes"
 import { Button } from "@/components/ui/button"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
 
 export default function ThemeSwitch() {
   const { theme, setTheme } = useTheme()
@@ -15,10 +21,19 @@ export default function ThemeSwitch() {
     }
   }
   return (
-    <Button variant="ghost" size="icon" onClick={() => changeTheme()}>
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      <span className="sr-only">Toggle theme</span>
-    </Button>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button variant="ghost" size="icon" onClick={() => changeTheme()}>
+            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span className="sr-only">Toggle theme</span>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          Switch to {theme === "dark" ? "light" : "dark"} theme
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   )
 }
